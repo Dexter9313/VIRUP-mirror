17aee5a83d140db9f8e023372a2dbabfc50d72dc
Florian Cabot <florian.cabot@epfl.ch>
2019-02-11 18:44:29 +0100

    Fixed merge problems

--- include/MainWin.hpp
+++ include/MainWin.hpp
@@ -33,8 +33,6 @@ class MainWin : public AbstractMainWin
 	// (no controllers or hands)
 	virtual void renderScene(BasicCamera const& camera) override;
 
-	virtual void applyPostProcShaderParams(QString const& id, GLHandler::ShaderProgram shader) const override;
-
   private:
 	QMatrix4x4 computeCubeModel() const;
 	void rescaleCube(float newScale, QVector3D const& scaleCenter);
