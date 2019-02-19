cbd43eaf9bfc3787cf1ae01e1cd794ed3c372f1b
Florian Cabot <florian.cabot@epfl.ch>
2019-02-19 14:51:43 +0100

    Added a way to specify additionnal dependencies

---
+++ example/cmake/Dependencies.cmake
@@ -0,0 +1,7 @@
+##EXAMPLE for Boost Library
+
+#find_package(Boost REQUIRED)
+
+## These variables must be set by the end of this file
+#set(PROJECT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
+#set(PROJECT_LIBRARIES ${Boost_LIBRARIES})
