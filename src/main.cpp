db9760c3ec747bb35f2d8927d2b6908ba19a3de6
Florian Cabot <florian.cabot@epfl.ch>
2019-02-21 18:12:09 +0100

    First step towards Python API implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QProcess>
 #include <QSettings>
 #ifdef Q_OS_UNIX
 #include <QDir>
 #include <unistd.h>
 #endif
+
+#include <PythonQt.h>
+#include <gui/PythonQtScriptingConsole.h>
 
 #include "Launcher.hpp"
 #include "MainWin.hpp"
@@ -21,6 +25,18 @@ int main(int argc, char* argv[])
 
 	QApplication a(argc, argv);
 
+	// init PythonQt and Python
+	PythonQt::init(PythonQt::IgnoreSiteModule | PythonQt::RedirectStdOut);
+
+	// get the __main__ python module
+	PythonQtObjectPtr mainModule = PythonQt::self()->getMainModule();
+
+	QProcess process;
+	process.start("python3 -c \"import sys;print(sys.path)\"");
+	process.waitForFinished(-1);
+	QString syspath(process.readAllStandardOutput());
+	mainModule.evalScript("import sys\nsys.path=" + syspath + "\ndel sys");
+
 	{
 		Launcher launcher;
 		launcher.init();
@@ -29,6 +45,10 @@ int main(int argc, char* argv[])
 	}
 
 	MainWin w;
+	PythonQtScriptingConsole console(nullptr, mainModule);
+	console.show();
+	mainModule.addObject("mainwin", &w);
+
 	if(QSettings().value("window/fullscreen").toBool())
 		w.showFullScreen();
 	else
