04983c389ff95a52657a5a584e7c175bef81f96c
Florian Cabot <florian.cabot@epfl.ch>
2019-03-01 16:58:28 +0100

    Added clang-tidy support

--- cmake/ClangDevTools.cmake
+++ cmake/ClangDevTools.cmake
@@ -20,16 +20,16 @@
 
 # Get all project files
 
-if(PROJECT_CHECK_FORMAT)
-	file(GLOB_RECURSE ALL_SOURCE_FILES ${HPP_FILES} ${SRC_FILES} ${MAIN_FILE} ${TEST_HPP_FILES} ${TEST_SRC_FILES})
-else()
-	file(GLOB_RECURSE ALL_SOURCE_FILES ${ENGINE_HPP_FILES} ${ENGINE_SRC_FILES} ${MAIN_FILE} ${ENGINE_TEST_HPP_FILES} ${ENGINE_TEST_SRC_FILES})
-endif()
-list(REMOVE_DUPLICATES ALL_SOURCE_FILES)
-
 find_package(ClangFormat)
 
 if(CLANG_FORMAT_FOUND)
+	if(PROJECT_CHECK_FORMAT)
+		file(GLOB_RECURSE ALL_FILES ${HPP_FILES} ${TEST_HPP_FILES} ${SRC_FILES} ${MAIN_FILE} ${TEST_SRC_FILES})
+	else()
+		file(GLOB_RECURSE ALL_FILES ${ENGINE_HPP_FILES} ${ENGINE_TEST_HPP_FILES} ${ENGINE_SRC_FILES} ${MAIN_FILE} ${ENGINE_TEST_SRC_FILES})
+	endif()
+	list(REMOVE_DUPLICATES ALL_FILES)
+
 	#Only get first executable
 	string(REGEX REPLACE ";.*$" "" CLANG_FORMAT "${CLANG_FORMAT_EXECUTABLE}")
 	message("clang-format executable: ${CLANG_FORMAT}")
@@ -38,8 +38,35 @@ if(CLANG_FORMAT_FOUND)
 		clang-format
 		COMMAND ${PROJECT_SOURCE_DIR}/ci/clang-format-report.sh
 		${CLANG_FORMAT}
-		${ALL_SOURCE_FILES}
+		${ALL_FILES}
 	)
 else()
 	message("clang-format executable not found")
+endif()
+
+find_package(ClangTidy)
+
+if(CLANG_TIDY_FOUND)
+	if(PROJECT_CHECK_TIDY)
+		file(GLOB_RECURSE ALL_SOURCE_FILES ${SRC_FILES} ${MAIN_FILE} ${TEST_SRC_FILES})
+	else()
+		file(GLOB_RECURSE ALL_SOURCE_FILES ${ENGINE_SRC_FILES} ${MAIN_FILE} ${ENGINE_TEST_SRC_FILES})
+	endif()
+	list(REMOVE_DUPLICATES ALL_SOURCE_FILES)
+
+	#Only get first executable
+	string(REGEX REPLACE ";.*$" "" CLANG_TIDY "${CLANG_TIDY_EXECUTABLE}")
+	message("clang-tidy executable: ${CLANG_TIDY}")
+	message("clang-tidy version: ${CLANG_TIDY_VERSION}")
+	add_custom_target(
+		clang-tidy
+		COMMAND ${PROJECT_SOURCE_DIR}/ci/clang-tidy-report.sh
+		${CLANG_TIDY}
+		-p .
+		${ALL_SOURCE_FILES}
+		-system-headers=0
+		-extra-arg=-Wno-unknown-warning-option
+	)
+else()
+	message("clang-tidy executable not found")
 endif()
