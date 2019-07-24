0e181bef448d884e7fa36f60ebb734dc0d7f39cb
Florian Cabot <florian.cabot@epfl.ch>
2019-07-24 17:27:24 +0200

    Add QWidget 3D rendering

---
+++ include/Widget3D.hpp
@@ -0,0 +1,60 @@
+/*
+    Copyright (C) 2019 Florian Cabot <florian.cabot@hotmail.fr>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef WIDGET3D_H
+#define WIDGET3D_H
+
+#include <QImage>
+#include <QPainter>
+#include <QWidget>
+
+#include "GLHandler.hpp"
+#include "Primitives.hpp"
+
+class Widget3D
+{
+  public:
+	explicit Widget3D(QWidget* widget);
+	QMatrix4x4 const& getModel() const { return model; };
+	QMatrix4x4& getModel() { return model; };
+	QImage const getImage() const { return image; };
+	QWidget& getWidget() const { return *widget; };
+	void setWidget(QWidget* widget);
+	void render();
+	~Widget3D();
+
+	static void paintWidget(QImage& image, QWidget& widget);
+
+  private:
+	void updateTex();
+
+	GLHandler::ShaderProgram shader = GLHandler::newShader("billboard");
+	GLHandler::Mesh quad            = Primitives::newQuad(shader);
+	GLHandler::Texture tex          = GLHandler::newTexture(0, 0, nullptr);
+
+	QMatrix4x4 model;
+
+	QMatrix4x4 aspectratio;
+
+	QSize originalSize;
+	QImage image;
+
+	QWidget* widget = nullptr;
+};
+
+#endif // WIDGET3D_H
---
+++ src/Widget3D.cpp
@@ -0,0 +1,80 @@
+/*
+    Copyright (C) 2019 Florian Cabot <florian.cabot@hotmail.fr>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "Widget3D.hpp"
+
+Widget3D::Widget3D(QWidget* widget)
+{
+	setWidget(widget);
+}
+
+void Widget3D::setWidget(QWidget* widget)
+{
+	this->widget = widget;
+	widget->setVisible(true);
+	widget->setVisible(false);
+	unsigned int width(widget->width()), height(widget->height());
+	originalSize = widget->size();
+	if(width > height)
+	{
+		aspectratio.scale(1.f, static_cast<float>(height) / width);
+	}
+	else
+	{
+		aspectratio.scale(static_cast<float>(width) / height, 1.f);
+	}
+	updateTex();
+}
+
+void Widget3D::render()
+{
+	GLHandler::setUpRender(shader, model * aspectratio);
+	GLHandler::useTextures({tex});
+	GLHandler::render(quad, GLHandler::PrimitiveType::TRIANGLE_STRIP);
+}
+
+void Widget3D::updateTex()
+{
+	GLHandler::deleteTexture(tex);
+
+	image = QImage(originalSize, QImage::Format_ARGB32);
+
+	paintWidget(image, *widget);
+
+	tex = GLHandler::newTexture(image);
+}
+
+Widget3D::~Widget3D()
+{
+	GLHandler::deleteTexture(tex);
+	GLHandler::deleteMesh(quad);
+	GLHandler::deleteShader(shader);
+}
+
+void Widget3D::paintWidget(QImage& image, QWidget& widget)
+{
+	auto painter = new QPainter(&image);
+	painter->setRenderHint(QPainter::Antialiasing);
+
+	// image.fill(QColor(0, 0, 0, 0));
+	widget.render(painter);
+
+	// The QPainter doesn't like its QImage to be changed
+	delete painter;
+	image = image.mirrored(false, true);
+}
