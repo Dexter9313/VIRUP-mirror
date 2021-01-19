f7214d476307d17d680972208c562d0758d0ebde
Florian Cabot <florian.cabot@epfl.ch>
2021-01-19 12:25:34 +0100

    Update liboctree version

--- virup/ci/travis/install.sh
+++ virup/ci/travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-	wget --content-disposition https://gitlab.com/Dexter9313/octree-file-format/-/jobs/artifacts/1.7.1/raw/liboctree-1.7.1-linux_amd64.deb?job=pack:liboctree ;
+	wget --content-disposition https://gitlab.com/Dexter9313/octree-file-format/-/jobs/artifacts/1.12.0/raw/liboctree-1.12.0-linux_amd64.deb?job=pack:liboctree ;
 	sudo dpkg -i ./*.deb ;
 	sudo apt-get install libboost-dev ;
 fi
